cb0310ef98583164ecf1e9f6c2033bf4fbde9fc9
hereiam123 <bdemaio1@gmail.com>
2018-09-05 20:53:52 -0400

    Completed create invoice component view

--- src/components/CreateInvoice.vue
+++ src/components/CreateInvoice.vue
@@ -82,3 +82,81 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "CreateInvoice",
+  data() {
+    return {
+      invoice: {
+        name: "",
+        total_price: 0
+      },
+      transactions: [],
+      nextTxnId: 1,
+      loading: "",
+      status: ""
+    };
+  },
+  methods: {
+    saveTransaction() {
+      // append data to the arrays
+      let name = document.getElementById("txn_name_modal").value;
+      let price = document.getElementById("txn_price_modal").value;
+
+      if( name.length != 0 && price > 0){
+        this.transactions.push({
+          id: this.nextTxnId,
+          name: name,
+          price: price
+        });
+        this.nextTxnId++;
+        this.calcTotal();
+        // clear their values
+        document.getElementById("txn_name_modal").value = "";
+        document.getElementById("txn_price_modal").value = "";
+      }
+    },
+    deleteTransaction(id) {
+      let newList = this.transactions.filter(function(el) {
+        return el.id !== id;
+      });
+      this.nextTxnId--;
+      this.transactions = newList;
+      this.calcTotal();
+    },
+    calcTotal(){
+      let total = 0;
+      this.transactions.forEach(element => {
+        total += parseInt(element.price);
+      });
+      this.invoice.total_price = total;
+    },
+    onSubmit() {
+      const formData = new FormData();
+      // format for request
+      let txn_names = [];
+      let txn_prices = [];
+      this.transactions.forEach(element => {
+        txn_names.push(element.name);
+        txn_prices.push(element.price);
+      });
+      formData.append("name", this.invoice.name);
+      formData.append("txn_names", txn_names);
+      formData.append("txn_prices", txn_prices);
+      formData.append("user_id", this.$route.params.user.id);
+      this.loading = "Creating Invoice, please wait ...";
+      // Post to server
+      axios.post("http://localhost:3128/invoice", formData).then(res => {
+        // Post a status message
+        this.loading = "";
+        if (res.data.status == true) {
+          this.status = res.data.message;
+        } else {
+          this.status = res.data.message;
+        }
+      });
+    }
+  }
+};
